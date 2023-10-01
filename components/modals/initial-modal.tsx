7ab85c7a31a357bfe9414e71a20e33ce7d37065d
priyansh <devanshsingh2222@gmail.com>
2023-10-01 17:01:53 +0530

    feat: navigation sidebar

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,8 +1,9 @@
 "use client";
-
+import axios from 'axios';
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from 'next/navigation';
 
 import {
     Dialog,
@@ -37,6 +38,8 @@ const formSchema = z.object({
 export const  InitialModal = () => {
     const [isMounted , setIsMounted] = useState(false);
 
+    const router = useRouter();
+
     useEffect(() => {
         setIsMounted(true);
     } , []);
@@ -52,7 +55,15 @@ export const  InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values : z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers" , values);
+
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        } catch(error) {
+            console.log(error);
+        }
     }
 
     if(!isMounted){
