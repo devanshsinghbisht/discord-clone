916a1cc2e8af3fe97c68c050355167fece70f073
Devansh Singh Bisht <Devanshsingh2222@gmail.com>
2024-03-31 15:48:35 +0530

    feat: invite code for servers

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,22 +1,24 @@
-"use client"
+"use client";
 
-import { CreateServerModal } from "@/components/modals/create-server-modal"
-import { useState , useEffect } from "react";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { useState, useEffect } from "react";
+import { InviteModal } from "@/components/modals/invite-modal";
 
 export const ModalProvider = () => {
-    const [isMounted , setIsMounter] = useState(false);
+  const [isMounted, setIsMounter] = useState(false);
 
-    useEffect(() => {
-        setIsMounter(true);
-    } , []);
+  useEffect(() => {
+    setIsMounter(true);
+  }, []);
 
-    if(!isMounted){
-        return null;
-    }
-    
-    return (
-        <>
-            <CreateServerModal />
-        </>
-    )
-}
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+    </>
+  );
+};
