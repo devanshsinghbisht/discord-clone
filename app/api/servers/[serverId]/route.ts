bec62c7f67d43dd07763cb59bf010e534cf095c2
Devansh Singh Bisht <Devanshsingh2222@gmail.com>
2024-04-05 15:21:01 +0530

    feat: manage members functionality

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,13 +2,16 @@ import { NextResponse } from "next/server";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+import { ProfileDetails, ServerWithMembersWithProfiles } from "@/types";
 
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string } }
 ) {
   try {
-    const profile = currentProfile();
+    const { profile } = currentProfile() as unknown as {
+      profile: ProfileDetails;
+    };
     const { name, imageUrl } = await req.json();
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
--- types.ts
+++ types.ts
@@ -1,5 +1,8 @@
-import { Server , Member , Profile } from "@prisma/client"
+import { Server, Member, Profile } from "@prisma/client";
 
 export type ServerWithMembersWithProfiles = Server & {
-    members:(Member & {profile : Profile})[];
+  members: (Member & { profile: Profile })[];
+};
+export type ProfileDetails = Profile & {
+  id: String;
 };
