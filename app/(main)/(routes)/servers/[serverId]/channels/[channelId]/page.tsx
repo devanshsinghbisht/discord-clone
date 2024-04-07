11decd0576b4a5a37f2ae71b02d6d000929161d1
Devansh Singh Bisht <Devanshsingh2222@gmail.com>
2024-04-07 15:11:21 +0530

    feat: Chat Header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,48 @@
+import { redirectToSignUp } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import React from "react";
 
-const ChannelIdPage = () => {
-  return <div>Channel ID Page</div>;
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignUp();
+  }
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
---
+++ components/chat/chat-header.tsx
@@ -0,0 +1,26 @@
+import { Hash, Menu } from "lucide-react";
+import { MobileToggle } from "@/components/mobile-toggle";
+
+interface ChatHeaderProp {
+  serverId: string;
+  name: string;
+  type: "channel" | "conversation";
+  imageUrl?: string;
+}
+
+export const ChatHeader = ({
+  serverId,
+  name,
+  type,
+  imageUrl,
+}: ChatHeaderProp) => {
+  return (
+    <div className="text-md font-semibold flex px-3 items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      <MobileToggle serverId={serverId} />
+      {type === "channel" && <Hash className="h-5 w-5 text-zinc-400 mr-2" />}
+      <p className="font-semibold text-zinc-500 dark:text-zinc-400 mr-2">
+        {name}
+      </p>
+    </div>
+  );
+};
